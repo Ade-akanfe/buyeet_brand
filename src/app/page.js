90e1df2ea139bd5a6239b5d1185172550ccc5047
Ade-akanfe <aadeakanfe@gmail.com>
2024-12-08 07:05:21 +0100

    testing tailwind

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,6 @@
 export default function Home() {
   return (
     // <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-    <div>Welcome to the buyeet brand landing page.</div>
+    <div className="w-full h-screen flex justify-center items-center text-xs sm:text-sm md:text-base lg:text-2xl  ">Welcome to the buyeet brand landing page.</div>
   );
 }
